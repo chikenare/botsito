f9485761a8f6cf3a45daa1bea9e3c48dac505d23
Cristian <chikenare@gmail.com>
2025-10-17 17:12:18 -0600

    best search

--- lib/pages/home/widgets/search_input.dart
+++ lib/pages/home/widgets/search_input.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:botsito/providers/source_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SearchInput extends HookConsumerWidget {
@@ -8,13 +11,26 @@ class SearchInput extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final searchNotifier = ref.read(searchProvider.notifier);
+    final searchText = useState('');
+    final debounceTimer = useState<Timer?>(null);
+
+    useEffect(() {
+      return () {
+        debounceTimer.value?.cancel();
+      };
+    }, []);
+
+    void onChanged(String value) {
+      if (value.isEmpty) return;
+      searchText.value = value;
+      debounceTimer.value?.cancel();
+      debounceTimer.value = Timer(const Duration(milliseconds: 500), () {
+        searchNotifier.search(value);
+      });
+    }
 
     return TextField(
-      onChanged: (value) {
-        if (value.length >= 3) {
-          searchNotifier.search(value);
-        }
-      },
+      onChanged: onChanged,
       decoration: InputDecoration(
         hintText: 'Buscar',
         prefixIcon: Icon(Icons.search),
